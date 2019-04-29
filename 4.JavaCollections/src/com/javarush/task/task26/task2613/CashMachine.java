bbcbba6c67c5cbef280a5401af64e1f17c8f64e4
Tor <gor_ivanov@live.ru>
2019-04-29 16:37:37 +0300

    .

--- 4.JavaCollections/src/com/javarush/task/task26/task2613/CashMachine.java
+++ 4.JavaCollections/src/com/javarush/task/task26/task2613/CashMachine.java
@@ -1,5 +1,7 @@
 package com.javarush.task.task26.task2613;
 
+import jdk.nashorn.internal.objects.annotations.Constructor;
+
 public class CashMachine {
 
     public static void main(String[] args) {
